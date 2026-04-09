eb6ed8428c61f673e9407904e81f942ab2bc6c5b
mintchocobubbletea <98091021+mintchocobubbletea@users.noreply.github.com>
2026-04-09 11:20:55 +0900

    과제 1 코딩과 테스트 완료
    기본 UI 구성 완료 및 주문하기 버튼, 초기화 버튼 구현 완료

--- Form1.cs
+++ Form1.cs
@@ -6,10 +6,45 @@ namespace BurgerKiosk
         {
             InitializeComponent();
         }
-
+        int totalCost = 0;
         private void btnOrder_Click(object sender, EventArgs e)
         {
-
+            if (rdoHamBurger.Checked)
+            {
+                totalCost += 5000;
+                lstOrder.Items.Add("햄버거 5,000원");
+            }
+            else if (rdoBulgogiBurger.Checked)
+            {
+                totalCost += 4000;
+                lstOrder.Items.Add("불고기버거 4,000원");
+            }
+            else if (rdoChickenBurger.Checked)
+            {
+                totalCost += 3000;
+                lstOrder.Items.Add("치킨버거 3,000원");
+            }
+            if (chkPotato.Checked)
+            {
+                totalCost += 3500;
+                lstOrder.Items.Add("감자튀김 3,500원");
+            }
+            if (chkCola.Checked)
+            {
+                totalCost += 2500;
+                lstOrder.Items.Add("콜라 2,500원");
+            }
+            if (chkCheese.Checked)
+            {
+                totalCost += 1500;
+                lstOrder.Items.Add("치즈 1,500원");
+            }
+            if (chkSauce.Checked)
+            {
+                totalCost += 500;
+                lstOrder.Items.Add("소스 500원");
+            }
+            lblTotalCost.Text = "총 금액 : " + totalCost.ToString("N0") + "원";
         }
 
         private void btnInit_Click(object sender, EventArgs e)
@@ -22,6 +57,7 @@ namespace BurgerKiosk
             chkCheese.Checked = false;
             chkSauce.Checked = false;
             lstOrder.Items.Clear();
+            totalCost = 0;
             lblTotalCost.Text = "총 금액 : 0원";
         }
     }
